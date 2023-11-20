1980cac243e061003ff08eab3c48e3175dc65e83
mernamedhat <marmedhat382@gmail.com>
2023-11-20 02:03:17 +0200

    first commit

---
+++ app/src/main/java/com/example/testkotlin/splashActivity.kt
@@ -0,0 +1,24 @@
+package com.example.testkotlin
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
+import android.view.WindowManager
+
+class splashActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash)
+        window.setFlags(
+            WindowManager.LayoutParams.FLAG_FULLSCREEN,
+            WindowManager.LayoutParams.FLAG_FULLSCREEN
+        )
+        Handler(Looper.getMainLooper()).postDelayed({
+            val intent = Intent(this, MainActivity3::class.java)
+            startActivity(intent)
+            finish()
+        }, 3000)
+    }
+}
